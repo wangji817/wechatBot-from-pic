9d129c109725e6e03683382792ce2563cc2baed5
wangji817 <526153861@qq.com>
2022-06-16 17:53:10 +0800

    Update index_text.js

--- config/index_text.js
+++ config/index_text.js
@@ -1,14 +1,14 @@
 // 配置文件
 module.exports = {
     // 基础定时发送功能配置项（必填项）    
-    NICKNAME1: '前端开发-软通', //昵称    
-    NICKNAME2: 'H5开发小组',
-    NICKNAME3: '每日建单提醒',
+    NICKNAME1: '公司群名1', //昵称    
+    NICKNAME2: '公司群名2',
+    NICKNAME3: '公司群名3',
     // SENDDATE: '00 24 17 * * 4', //定时发送时间 每天9点0分0秒发送，规则见 /schedule/index.js    
     SENDDATE1: '00 30 17 * * 5', //定时发送时间 每分钟内的30秒发送，规则见 /schedule/index.js    每周五 17点30分发送    
     SENDDATE4: '00 30 09 * * {day}', //定时发送时间 每分钟内的30秒发送，规则见 /schedule/index.js    每周一至周五 9点30分发送
     SENDDATE5: '00 30 17 * * {day}', //定时发送时间 每分钟内的30秒发送，规则见 /schedule/index.js    每周一至周五 17点30分发送    
     SECONDDATE: '30 * * * * *', //测试
-    roomDesc1: '@所有人<br>{time}<br>大家好，本周周报记得下班前写一下 <br>https://docs.qq.com/sheet/DSlZTZXhnUGhtS1Bu?tab=BB08J2<br>新人可在本周日期内新增一行，格式：参照其他同学(导师负责新人指导一下)<br>每周壹句：{oneday}',
-    roomDesc2: "@所有人<br>{time}<br>大家好，大家单子Jira子任务别忘记创建~~~",
+    roomDesc1: '@所有人<br>{time}<br>大家好，<br>每周壹句：{oneday}',
+    roomDesc2: "@所有人<br>{time}<br>大家好，核酸记得不要忘记做哦~~",
 }   
